929bbe3f8565b5635fe8166dc965cf1f9cb17954
panderonak <pandeyronak139@gmail.com>
2025-05-19 21:27:29 +0530

    feat(auth): extend NextAuth types for custom user fields and roles.

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,8 +1,34 @@
-import { DefaultSession } from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import type { NextRequest } from "next/server";
+import { JWT } from "next-auth/jwt";
+import { UserRole } from "@prisma/client";
 
 declare module "next/server" {
   interface NextRequest {
     auth: DefaultSession | null;
+  }
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      address: string;
+      id?: string;
+      username?: string;
+      role?: UserRole;
+    } & DefaultSession["user"];
+  }
+  interface User {
+    id?: string;
+    username?: string;
+    role?: UserRole;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id?: string;
+    username?: string;
+    role?: UserRole;
   }
 }
