993d71a8222650982d0d4dc0980aed0aa7d370a7
panderonak <pandeyronak139@gmail.com>
2025-05-21 21:51:33 +0530

    feat(auth): implement social sign-in with Google and GitHub.

--- src/components/auth/socials.tsx
+++ src/components/auth/socials.tsx
@@ -1,14 +1,20 @@
+"use client";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { signIn } from "@/auth";
 
 export function Socials() {
+  const handleSocialSignIn = async (provider: "google" | "github") => {
+    await signIn(provider);
+  };
   return (
     <div className="flex items-center w-full gap-x-2">
       <Button
         size={"lg"}
         variant={"outline"}
         className="w-1/2 rounded-3xl cursor-pointer"
+        onClick={() => handleSocialSignIn("google")}
       >
         <FcGoogle className="h-5 w-5" />
         <span>Google</span>
@@ -17,6 +23,7 @@ export function Socials() {
         size={"lg"}
         variant={"outline"}
         className="w-1/2 rounded-3xl cursor-pointer"
+        onClick={() => handleSocialSignIn("github")}
       >
         <FaGithub className="h-5 w-5" />
         <span>Git Hub</span>
