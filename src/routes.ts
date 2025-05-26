92158dc586918e3da1cb8499f71014ae0ce1dd7d
panderonak <pandeyronak139@gmail.com>
2025-05-26 16:34:33 +0530

    feat(auth): add verification route to authRoutes and broaden API auth prefix.

--- src/routes.ts
+++ src/routes.ts
@@ -4,11 +4,15 @@ export const publicRoutes: string[] = ["/"];
 
 // An array of routes that are used for authentication.
 // These routes will redirect authenticated users to "/settings".
-export const authRoutes: string[] = ["/sign-in", "/sign-up"];
+export const authRoutes: string[] = [
+  "/sign-in",
+  "/sign-up",
+  "/verification(?:/[^/]+)?",
+];
 
 // The prefix for API authentication routes.
 // Routes that start with this prefix are used for API authentication purposes.
-export const APIAuthPrefix: string = "/api/auth";
+export const APIAuthPrefix: string = "/api";
 
 // The default redirect path after SIGN UP.
 export const DEFAULT_SIGNIN_REDIRECT: string = "/settings";
