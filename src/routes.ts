e8acdb4544cd177f12e4a5e426451473dd21e582
panderonak <pandeyronak139@gmail.com>
2025-06-01 17:35:24 +0530

    feat(routes): classify  as an auth route.

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,19 @@
 // An array of routes that are accessible to the public.
 // These routes do not require authentication.
-export const publicRoutes: string[] = ["/"];
+export const publicRoutes: string[] = ['/'];
 
 // An array of routes that are used for authentication.
 // These routes will redirect authenticated users to "/settings".
 export const authRoutes: string[] = [
-  "/sign-in",
-  "/sign-up",
-  "/verification(?:/[^/]+)?",
+  '/sign-in',
+  '/sign-up',
+  '/verification(?:/[^/]+)?',
+  '/reset-password(?:/[^/]+)?',
 ];
 
 // The prefix for API authentication routes.
 // Routes that start with this prefix are used for API authentication purposes.
-export const APIAuthPrefix: string = "/api";
+export const APIAuthPrefix: string = '/api';
 
 // The default redirect path after SIGN UP.
-export const DEFAULT_SIGNIN_REDIRECT: string = "/settings";
+export const DEFAULT_SIGNIN_REDIRECT: string = '/settings';
