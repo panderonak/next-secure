30f53cfe62c1a96507ea189e3e5acf6b738b6706
panderonak <pandeyronak139@gmail.com>
2025-06-01 17:23:00 +0530

    refactor(auth): improve error handling and support password recovery via link.

--- src/components/auth/sign-in-form.tsx
+++ src/components/auth/sign-in-form.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { CardWrapper } from "@/components/auth/card-wrapper";
-import { signInSchema } from "@/schemas/sign-in-schema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
+import { CardWrapper } from '@/components/auth/card-wrapper';
+import { signInSchema } from '@/schemas/sign-in-schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 import {
   Form,
   FormControl,
@@ -12,60 +12,78 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-  FormDescription,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { FormError } from "@/components/form-feedback/form-error";
-import { FormSuccess } from "@/components/form-feedback/form-success";
-import { useState, useTransition } from "react";
-import { useRouter } from "next/navigation";
-import { useSearchParams } from "next/navigation";
-import { signIn } from "next-auth/react";
-// import { signIn } from '@/auth';
-import { BeatLoader } from "react-spinners";
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { FormError } from '@/components/form-feedback/form-error';
+import { FormSuccess } from '@/components/form-feedback/form-success';
+import { useState, useTransition } from 'react';
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
+import { signIn } from 'next-auth/react';
+import { BeatLoader } from 'react-spinners';
+import Link from 'next/link';
+import { AxiosError } from 'axios';
+import APIResponseInterface from '@/types/APIResponseInterface';
 
 export function SignInForm() {
   const form = useForm<z.infer<typeof signInSchema>>({
     resolver: zodResolver(signInSchema),
     defaultValues: {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     },
   });
 
   const { reset } = form;
 
   const [isPending, startTransition] = useTransition();
-  const [error, setError] = useState<string | undefined>("");
-  const [success, setSuccess] = useState<string | undefined>("");
+  const [error, setError] = useState<string | undefined>('');
+  const [success, setSuccess] = useState<string | undefined>('');
 
   const router = useRouter();
 
   const searchParams = useSearchParams();
   const urlError =
-    searchParams.get("error") === "OAuthAccountNotLinked"
-      ? "This email is linked to a different sign-in method."
-      : "";
+    searchParams.get('error') === 'OAuthAccountNotLinked'
+      ? 'This email is linked to a different sign-in method.'
+      : '';
 
   const onSubmit = (values: z.infer<typeof signInSchema>) => {
-    setError("");
-    setSuccess("");
+    setError('');
+    setSuccess('');
     startTransition(async () => {
-      const response = await signIn("credentials", {
-        redirect: false,
-        ...values,
-      });
+      try {
+        const response = await signIn('credentials', {
+          redirect: false,
+          ...values,
+        });
 
-      if (response?.error) {
-        setError(
-          response?.error?.replace(/^Error:\s*/, "") || "Incorrect credentials."
-        );
-      }
+        if (response?.error) {
+          setError(
+            response?.error?.replace(/^Error:\s*/, '') ||
+              'Incorrect credentials.'
+          );
+        }
 
-      if (response?.url) {
-        router.replace("/settings");
-        setSuccess(response.message);
+        if (response?.url) {
+          setSuccess('Signed in successfully!');
+          router.replace('/settings');
+        }
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error(
+            `Error occurred while sign in. Error details: ${error}. Stack trace: ${error.stack || 'No stack trace available'}`
+          );
+        }
+        const axiosError = error as AxiosError<APIResponseInterface>;
+        const errorMessage =
+          axiosError.response?.data.message ||
+          'Something went wrong. Please try again.';
+
+        setError(errorMessage);
+      } finally {
+        reset();
       }
     });
   };
@@ -94,9 +112,6 @@ export function SignInForm() {
                     disabled={isPending}
                   />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
@@ -115,7 +130,14 @@ export function SignInForm() {
                     disabled={isPending}
                   />
                 </FormControl>
-                <FormDescription>This is your password.</FormDescription>
+                <Button
+                  size="sm"
+                  variant="link"
+                  className="px-0 font-normal w-1/2"
+                  asChild
+                >
+                  <Link href={'/reset-password'}>Forgot Password?</Link>
+                </Button>
                 <FormMessage />
               </FormItem>
             )}
@@ -124,7 +146,7 @@ export function SignInForm() {
           <FormSuccess message={success} />
 
           <Button type="submit" disabled={isPending} className="w-full">
-            {isPending ? <BeatLoader color="#fff" size={5} /> : "Sign In"}
+            {isPending ? <BeatLoader color="#fff" size={5} /> : 'Sign In'}
           </Button>
         </form>
       </Form>
